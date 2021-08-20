3e7c6bed2df9c2d1f85f3c925e2578e9888e08bb
murillojoaquim <murillojalves@hotmail.com>
2021-08-20 20:13:31 -0300

    Musica-Natural

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Black;
+            
+            Console.WriteLine("NATURAL - IMAGINE DRAGONS");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            //COR VERDE = PARTE MAIS CALMA DA MUSICA (DESENVOLVIMENTO), COR VERMELHA = PARTE MAIS AGRESSIVA DA MUSICA(REFRÃO)
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            Console.WriteLine("Will you hold the line");
+            Console.WriteLine("When every one of them has given up or given in? Tell me");
+            Console.WriteLine("In this house of mine");
+            Console.WriteLine("Nothing ever comes without a consequence or cost, tell me");
+            Console.WriteLine("Will the stars align?");
+            Console.WriteLine("Will heaven step in? Will it save us from our sin? Will it?");
+            Console.WriteLine("'Cause this house of mine stands strong\n");
+
+
+            Console.WriteLine("That's the price you pay");
+            Console.WriteLine("Leave behind your heart and cast away");
+            Console.WriteLine("Just another product of today");
+            Console.WriteLine("Rather be the hunter than the prey");
+            Console.WriteLine("And you're standing on the edge, face up\n");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("'Cause you're a natural");
+            Console.WriteLine("A beating heart of stone");
+            Console.WriteLine("You gotta be so cold");
+            Console.WriteLine("To make it in this world");
+            Console.WriteLine("Yeah, you're a natural");
+            Console.WriteLine("Living your life cutthroat");
+            Console.WriteLine("You gotta be so cold");
+            Console.WriteLine("Yeah, you're a natural\n");
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            Console.WriteLine("Will somebody");
+            Console.WriteLine("Let me see the light within the dark trees shadowing");
+            Console.WriteLine("What's happenin'?");
+            Console.WriteLine("Lookin' through the glass, find the wrong within the past, knowin'");
+            Console.WriteLine("Oh, we are the youth");
+            Console.WriteLine("Cut until it bleeds inside a world without the peace, face it");
+            Console.WriteLine("A bit of the truth, the truth\n");
+
+
+            Console.WriteLine("That's the price you pay");
+            Console.WriteLine("Leave behind your heart and cast away");
+            Console.WriteLine("Just another product of today");
+            Console.WriteLine("Rather be the hunter than the prey");
+            Console.WriteLine("And you're standing on the edge, face up\n");
+            
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("'Cause you're a natural");
+            Console.WriteLine("A beating heart of stone");
+            Console.WriteLine("You gotta be so cold");
+            Console.WriteLine("To make it in this world");
+            Console.WriteLine("Yeah, you're a natural");
+            Console.WriteLine("Living your life cutthroat");
+            Console.WriteLine("You gotta be so cold");
+            Console.WriteLine("Yeah, you're a natural\n");
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            Console.WriteLine("Deep inside me, I'm fading to black, I'm fading");
+            Console.WriteLine("Took an oath by the blood of my hand, won't break it");
+            Console.WriteLine("I can taste it, the end is upon us, I swear");
+            Console.WriteLine("Gonna make it");
+            Console.WriteLine("I'm gonna make it\n");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("Natural");
+            Console.WriteLine("A beating heart of stone");
+            Console.WriteLine("You gotta be so cold");
+            Console.WriteLine("To make it in this world");
+            Console.WriteLine("Yeah, you're a natural");
+            Console.WriteLine("Living your life cutthroat");
+            Console.WriteLine("You gotta be so cold");
+            Console.WriteLine("Yeah, you're a natural\n");
+
+            Console.WriteLine("Natural");
+            Console.WriteLine("Yeah you're natural");
+
+            Console.ResetColor();
+        }
+    }
+}
